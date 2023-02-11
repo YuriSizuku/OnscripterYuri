4cb51f98cd6d073fe82c31704de927b67d8bdade
devseed <devseed@yurisizuku.localdomain>
2023-02-11 21:39:16 +0900

    linux build

--- script/local_linux64.sh
+++ script/local_linux64.sh
@@ -1,13 +1,17 @@
-BUILD_PATH=./../build_linux32
+# sh -c "export BUILD_TYPE=Debug && ./local_linux64.sh"
+BUILD_PATH=./../build_linux64
 CMAKELISTS_PATH=./../
+CORE_NUM=$(cat /proc/cpuinfo | grep -c ^processor)
+TARGETS=$@
 
-# prepare libs
-if ! [ -d ./../externlib ]; then mkdir ./../externlib; fi
-if ! [ -d ./../externlib/stb ]; then git clone https://github.com/nothings/stb.git ./../externlib/stb; fi
+# config env
+CC=gcc
+CXX=g++
 
 # config and build project
+echo "BUILD_TYPE=$BUILD_TYPE"
 cmake -B $BUILD_PATH -S $CMAKELISTS_PATH \
-      -G "Unix Makefiles" \
-      -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
-      -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 
-make -C $BUILD_PATH all
+    -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
+    -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64
+make -C $BUILD_PATH $TARGETS -j$CORE_NUM
